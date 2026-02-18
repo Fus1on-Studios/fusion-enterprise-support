941da64d1f5385f1443ba51ed82038c81b21a1bb
Fus1on Studios <us@fus1on.live>
2026-02-17 20:35:05 -0700

    Update deploy-commands.js

--- services/bot-service/deploy-commands.js
+++ services/bot-service/deploy-commands.js
@@ -1,1 +1,24 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
 
+const commands = [];
+const folders = fs.readdirSync('./src/commands');
+
+for (const folder of folders) {
+  const files = fs.readdirSync(`./src/commands/${folder}`);
+  for (const file of files) {
+    const command = require(`./src/commands/${folder}/${file}`);
+    commands.push(command.data.toJSON());
+  }
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  await rest.put(
+    Routes.applicationCommands(process.env.CLIENT_ID),
+    { body: commands }
+  );
+  console.log('Slash commands deployed successfully.');
+})();
