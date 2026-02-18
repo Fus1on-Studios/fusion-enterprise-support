22a8e11d9672d5343057efad12f7f3a7fd357cbf
Fus1on Studios <us@fus1on.live>
2026-02-17 20:00:39 -0700

    Create tailwind.config.js

---
+++ web-panel/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        brand: process.env.NEXT_PUBLIC_BRAND_COLOR,
+        secondary: process.env.NEXT_PUBLIC_SECONDARY_COLOR
+      }
+    }
+  }
+}
