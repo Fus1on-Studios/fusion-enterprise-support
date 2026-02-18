7a4f2803139a10da11928d4ef5a826abc750abd6
Fus1on Studios <us@fus1on.live>
2026-02-17 20:34:03 -0700

    Update levelingService.js

--- services/bot-service/src/services/levelingService.js
+++ services/bot-service/src/services/levelingService.js
@@ -1,1 +1,19 @@
+const Level = require('../database/schemas/Level');
 
+async function addXP(userId, guildId, amount) {
+  let data = await Level.findOne({ userId, guildId });
+  if (!data) data = await Level.create({ userId, guildId, xp: 0, level: 1 });
+
+  data.xp += amount;
+
+  const needed = data.level * 100;
+  if (data.xp >= needed) {
+    data.level++;
+    data.xp = 0;
+  }
+
+  await data.save();
+  return data;
+}
+
+module.exports = { addXP };
