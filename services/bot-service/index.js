e60af2d5d2797e281c696c063787232b5cd62694
Fus1on Studios <us@fus1on.live>
2026-02-17 20:03:30 -0700

    Update index.js

--- services/bot-service/index.js
+++ services/bot-service/index.js
@@ -1,17 +1,35 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, Collection } from "discord.js";
+import fs from "fs";
+import path from "path";
 import dotenv from "dotenv";
 import { connectDB } from "../../shared/config/database.js";
-import Ticket from "../../shared/models/Ticket.js";
 
 dotenv.config();
 connectDB();
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
 });
 
+client.commands = new Collection();
+
+const eventsPath = path.join("./events");
+const eventFiles = fs.readdirSync(eventsPath);
+
+for (const file of eventFiles) {
+  const event = (await import(`./events/${file}`)).default;
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
 client.once("ready", () => {
-  console.log(`Bot logged in as ${client.user.tag}`);
+  console.log(`Bot ready: ${client.user.tag}`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
